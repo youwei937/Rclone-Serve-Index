1c9133cd87f4d115a0f69b108c1d838dcc516c6a
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-06-25 20:19:34 +0430

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 rclone version
 rclone listremotes
 #refreshing Rclone
-wget $CONFIG_IN_URL -O './.config/rclone/rclone.conf'
+wget $CONFIG_IN_URL -O '/root/.config/rclone/rclone.conf'
 rclone version
 rclone listremotes
 rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
